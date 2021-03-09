a0c108bc7c25a8916c22db742faf7de73db47534
ZhirenXu <xu.3579@buckeyemail.osu.edu>
2021-03-09 12:27:52 -0500

    Update FileDownloader.py

--- FileDownloader.py
+++ FileDownloader.py
@@ -3,14 +3,17 @@ import progressbar
 
 url = 'https://library.osu.edu/dc/downloads/hh63sw64g?locale=en'
 k = 0
+
 s = requests.session()
 file = s.get(url)
+
 fileSize = file.headers["Content-Length"]
 contentDisposition = file.headers["Content-Disposition"]
 contentDispositionLength = len(contentDisposition)
 fileNameIndex = contentDisposition.index("filename")
 fileName = contentDisposition[fileNameIndex+10 : contentDispositionLength-1]
 print(fileName)
+
 with open(fileName, 'wb') as outFile:
     #for k in progressbar.progressbar(range(int(fileSize)), redirect_stdout=True):
     outFile.write(file.content)
